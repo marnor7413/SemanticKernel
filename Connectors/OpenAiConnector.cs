6003cf58672029c904a6e849028565f211934712
Martin Norén <26736807+marnor7413@users.noreply.github.com>
2026-07-05 11:55:58 +0200

    refactor: enhance readability

--- Connectors/OpenAiConnector.cs
+++ Connectors/OpenAiConnector.cs
@@ -7,11 +7,16 @@ namespace LLMChatApp.Connectors;
 
 internal class OpenAiConnector
 {
+    private const string BeginningOfQuestion = "Me: ";
+    private const string BeginningOfReply = "AI: ";
+    private const string EndApplicationCommand = "quit";
+    private const string ExitApplicationMessage = "#### END OF CHAT";
+
     public async Task SimpleChat(Kernel kernel, OpenAIPromptExecutionSettings options)
     {
         var chatKlient = kernel.GetRequiredService<IChatCompletionService>();
 
-        string systemPrompt = @"Key Requirements:
+        const string SystemPrompt = @"Key Requirements:
             1. Always verify answers using available tools before responding.
             2. If you make an error, explain it and correct it immediately.
             3. All calculations must be accurate and logical.
@@ -22,31 +27,37 @@ internal class OpenAiConnector
 
             Critical: Your primary goal is to provide reliable, accurate, and correct responses at all times.";
         var history = new ChatHistory();
-        history.AddSystemMessage(systemPrompt);
+        history.AddSystemMessage(SystemPrompt);
 
         while (true)
         {
-            Console.Write("Me: ");
+            Console.Write(BeginningOfQuestion);
             var prompt = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(prompt) || prompt.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            if (ExitApplication(prompt))
             {
                 break;
             }
 
             history.AddUserMessage(prompt);
 
-            var completeResponse = new StringBuilder();
-            Console.Write("AI: ");
+            var LlmResponse = new StringBuilder();
+            Console.Write(BeginningOfReply);
             await foreach (var messageChunk in chatKlient.GetStreamingChatMessageContentsAsync(history, options, kernel))
             {
                 Console.Write(messageChunk.Content);
-                completeResponse.Append(messageChunk.Content);
+                LlmResponse.Append(messageChunk.Content);
             }
 
-            history.AddAssistantMessage(completeResponse.ToString());
+            history.AddAssistantMessage(LlmResponse.ToString());
             Console.WriteLine();
         }
 
-        Console.WriteLine("#### END OF CHAT");
+        Console.WriteLine();
+        Console.WriteLine(ExitApplicationMessage);
+    }
+
+    private static bool ExitApplication(string? prompt)
+    {
+        return string.IsNullOrWhiteSpace(prompt) || prompt.Equals(EndApplicationCommand, StringComparison.OrdinalIgnoreCase);
     }
 }
