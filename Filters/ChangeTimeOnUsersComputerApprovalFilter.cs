9a671e7b20518fcffe0ccadc0c137fcc65a8f009
Martin Norén <26736807+marnor7413@users.noreply.github.com>
2026-07-11 21:08:36 +0200

    feature: adds an approval filter for a dummy func
    A test to explore how user-in-the-loop could work for sensitive actions

---
+++ Filters/ChangeTimeOnUsersComputerApprovalFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace LLMChatApp.Filters;
+
+public class ChangeTimeOnUsersComputerApprovalFilter : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        if (context.Function.PluginName == "TimePlugin" 
+            && context.Function.Name == "ChangeTimeOnUsersComputer")
+        {
+            Console.WriteLine("The system wants to update your computers time. Proceed? (y/n):");
+            var answer = Console.ReadLine();
+
+            if (answer.Equals("y", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Updating the time was approved by the user!");
+                await next(context);
+            }
+
+            context.Result = new FunctionResult(context.Result, "Updating the time was not approved by the user!");
+            
+            return;
+        }
+    }
+}
--- Plugins/TimePlugin.cs
+++ Plugins/TimePlugin.cs
@@ -21,4 +21,12 @@ internal class TimePlugin
             .GetCurrentDateTime()
             .ToString("yyyy-MM-dd HH:mm:ss");
     }
+
+    [KernelFunction]
+    [Description("Changes the time on the users computer.")]
+    public string ChangeTimeOnUsersComputer(
+        [Description("The desired time that you want to change to on the users computer. Should be formatted yyyy-MM-dd HH:mm:ss.")]string newTime)
+    {
+        return $"Time on local computer was changed to {newTime}.";
+    }
 }
